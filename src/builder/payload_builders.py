ea2b925a9c527be3de2b349ff25f5516293d7600
Ivan William <ivanwilliam.md@gmail.com>
2026-08-29 10:39:32 +0700

    chore: remove old stub builders (patient/resources files)

--- src/builder/payload_builders.py
+++ src/builder/payload_builders.py
@@ -1,88 +1,389 @@
 class BaseBuilder:
-    def __init__(self, resource_type: str):
-        self.data = {"resourceType": resource_type}
 
-    def to_dict(self):
+    def __init__(self, resource_type: str):
+
+        self.resource_type = resource_type
+
+        self.data: dict = {}
+
+
+    def get_resource_type(self) -> str:
+
+        return self.resource_type
+
+
+    def to_json(self) -> dict:
+
         return self.data
 
-class PatientBuilder(BaseBuilder):
-    def __init__(self): super().__init__("Patient")
-    def set_nik(self, nik: str):
-        self.data["identifier"] = [{"system": "https://fhir.kemkes.go.id/id/nik", "value": nik}]
-        return self
-    def set_name(self, name: str):
-        self.data["name"] = [{"use": "official", "text": name}]
-        return self
-    def set_gender(self, gender: str):
-        self.data["gender"] = gender
-        return self
-    def set_birth_date(self, birth_date: str):
-        self.data["birthDate"] = birth_date
+
+    def set_id(self, id_val: str) -> "BaseBuilder":
+
+        self.data["id"] = id_val
+
         return self
 
-class PractitionerBuilder(BaseBuilder):
-    def __init__(self): super().__init__("Practitioner")
-    def set_nik(self, nik: str):
-        self.data["identifier"] = [{"system": "https://fhir.kemkes.go.id/id/nik", "value": nik}]
-        return self
-    def set_name(self, name: str):
-        self.data["name"] = [{"use": "official", "text": name}]
+
+    def add_meta(self, meta: dict) -> "BaseBuilder":
+
+        self.data["meta"] = meta
+
         return self
 
-class OrganizationBuilder(BaseBuilder):
-    def __init__(self): super().__init__("Organization")
-    def set_id(self, org_id: str):
-        self.data["id"] = org_id
-        return self
-    def set_name(self, name: str):
-        self.data["name"] = name
+
+    def add_extension(self, ext: str, val) -> "BaseBuilder":
+
+        self.data[ext] = val
+
         return self
 
-class LocationBuilder(BaseBuilder):
-    def __init__(self): super().__init__("Location")
-    def set_name(self, name: str):
-        self.data["name"] = name
-        return self
-    def set_managing_organization(self, org_ref: str):
-        self.data["managingOrganization"] = {"reference": org_ref}
-        return self
 
-class EncounterBuilder(BaseBuilder):
-    def __init__(self): super().__init__("Encounter")
-    def set_status(self, status: str):
-        self.data["status"] = status
-        return self
-    def set_subject(self, ref: str, display: str):
-        self.data["subject"] = {"reference": ref, "display": display}
-        return self
 
-class ConditionBuilder(BaseBuilder):
-    def __init__(self): super().__init__("Condition")
-    def set_clinical_status(self, code: str):
-        self.data["clinicalStatus"] = {
-            "coding": [{"system": "http://terminology.hl7.org/CodeSystem/condition-clinical", "code": code}]
-        }
-        return self
-    def set_subject(self, ref: str, display: str):
-        self.data["subject"] = {"reference": ref, "display": display}
-        return self
+class AccountBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Account')
+
+
+class AllergyIntoleranceBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('AllergyIntolerance')
+
+
+class BundleBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Bundle')
+
+
+class CarePlanBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('CarePlan')
+
+
+class ChargeItemBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('ChargeItem')
+
+
+class ChargeItemDefinitionBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('ChargeItemDefinition')
+
+
+class ChargeItemResponseBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('ChargeItemResponse')
+
 
 class ClaimBuilder(BaseBuilder):
+
     def __init__(self):
-        super().__init__("Claim")
-        
-    def set_status(self, status: str):
-        self.data["status"] = status
-        return self
-        
-    def set_use(self, use: str):
-        self.data["use"] = use
-        return self
+
+        super().__init__('Claim')
+
+
+class ClaimResponseBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('ClaimResponse')
+
+
+class ClinicalImpressionBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('ClinicalImpression')
+
+
+class CompositionBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Composition')
+
+
+class ConditionBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Condition')
+
 
 class CoverageBuilder(BaseBuilder):
+
     def __init__(self):
-        super().__init__("Coverage")
-        
-    def set_status(self, status: str):
-        self.data["status"] = status
-        return self
+
+        super().__init__('Coverage')
+
+
+class CoverageEligibilityRequestBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('CoverageEligibilityRequest')
+
+
+class CoverageEligibilityResponseBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('CoverageEligibilityResponse')
+
+
+class DeviceBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Device')
+
+
+class DiagnosticReportBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('DiagnosticReport')
+
+
+class DocumentReferenceBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('DocumentReference')
+
+
+class EncounterBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Encounter')
+
+
+class EpisodeOfCareBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('EpisodeOfCare')
+
+
+class FamilyMemberHistoryBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('FamilyMemberHistory')
+
+
+class GenomicStudyBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('GenomicStudy')
+
+
+class GoalBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Goal')
+
+
+class GroupBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Group')
+
+
+class ImagingStudyBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('ImagingStudy')
+
+
+class ImmunizationBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Immunization')
+
+
+class InvoiceBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Invoice')
+
+
+class LocationBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Location')
+
+
+class MedicationBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Medication')
+
+
+class MedicationAdministrationBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('MedicationAdministration')
+
+
+class MedicationDispenseBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('MedicationDispense')
+
+
+class MedicationRequestBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('MedicationRequest')
+
+
+class MedicationStatementBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('MedicationStatement')
+
+
+class MolecularSequenceBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('MolecularSequence')
+
+
+class NutritionOrderBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('NutritionOrder')
+
+
+class ObservationBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Observation')
+
+
+class OrganizationBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Organization')
+
+
+class PatientBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Patient')
+
+
+class PaymentNoticeBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('PaymentNotice')
+
+
+class PaymentReconciliationBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('PaymentReconciliation')
+
+
+class PractitionerBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Practitioner')
+
+
+class PractitionerRoleBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('PractitionerRole')
+
+
+class ProcedureBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Procedure')
+
+
+class QuestionnaireResponseBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('QuestionnaireResponse')
+
+
+class RelatedPersonBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('RelatedPerson')
+
+
+class RiskAssessmentBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('RiskAssessment')
+
+
+class ServiceRequestBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('ServiceRequest')
+
+
+class SpecimenBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Specimen')
+
+
+class SubstanceBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Substance')
+
+
+class TaskBuilder(BaseBuilder):
+
+    def __init__(self):
+
+        super().__init__('Task')
--- src/datatype/test_datatypes.py
+++ src/datatype/test_datatypes.py
@@ -1,6 +1,13 @@
-from src.datatype.datatypes import Identifier
+from src.datatype.datatypes import Identifier, Coding, CodeableConcept
 
 def test_identifier():
     ident = Identifier(system="https://fhir.kemkes.go.id/id/nik", value="987654321")
     assert ident.value == "987654321"
     assert ident.to_array()["value"] == "987654321"
+
+def test_coding():
+    c = Coding(system="http://snomed.info/sct", code="123", display="Test")
+    assert c.system == "http://snomed.info/sct"
+    result = c.to_array()
+    assert result["system"] == "http://snomed.info/sct"
+    assert result["code"] == "123"
